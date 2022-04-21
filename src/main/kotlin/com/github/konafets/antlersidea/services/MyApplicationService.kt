d55ace7ed86249ef2d7969663af8c64d023e4905
GitHub Action <action@github.com>
2022-04-21 10:49:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/konafets/antlersidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/konafets/antlersidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.konafets.antlersidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.konafets.antlersidea.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/konafets/antlersidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/konafets/antlersidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.konafets.antlersidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.konafets.antlersidea.MyBundle
 
 class MyProjectService(project: Project) {
 
