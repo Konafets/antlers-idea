df0cb78463382531ebd58a0c7bf8dd89aa4c8f11
Stefano Kowalke <info@arroba-it.de>
2022-05-05 14:54:58 +0200

    Add forgotten generated files for variable support

---
+++ src/main/gen/de/arrobait/antlers/psi/impl/AntlersVariableImpl.java
@@ -0,0 +1,30 @@
+// This is a generated file. Not intended for manual editing.
+package de.arrobait.antlers.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static de.arrobait.antlers.psi.AntlersTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import de.arrobait.antlers.psi.*;
+
+public class AntlersVariableImpl extends ASTWrapperPsiElement implements AntlersVariable {
+
+  public AntlersVariableImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull AntlersVisitor visitor) {
+    visitor.visitVariable(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof AntlersVisitor) accept((AntlersVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+}
