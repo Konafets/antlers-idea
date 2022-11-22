24ff39f87394e080d229bdac26fb12d7210ba0d4
Stefano Kowalke <info@arroba-it.de>
2022-11-22 19:56:33 +0100

    Remove useless comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    // Java support
     id("java")
-    // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.7.21"
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.10.0"
-    // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
-    // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
 }
 
