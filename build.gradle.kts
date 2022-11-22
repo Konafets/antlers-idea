788059910655bd77de18a0666bf8106368561b0d
Stefano Kowalke <info@arroba-it.de>
2022-11-22 12:50:02 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.8.1"
+    id("org.jetbrains.intellij") version "1.10.0"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.1"
+    id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
 }
@@ -64,7 +64,7 @@ sourceSets["main"].java.srcDirs("src/main/gen")
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.0")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.1")
 }
 
 tasks {
--- src/test/java/de/arrobait/antlers/parser/AntlersParserSpecTest.java
+++ src/test/java/de/arrobait/antlers/parser/AntlersParserSpecTest.java
@@ -1,102 +1,136 @@
 package de.arrobait.antlers.parser;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
 public class AntlersParserSpecTest extends AntlersParsingTest {
+    public AntlersParserSpecTest() {
+        super("parsing");
+    }
+
+    @Test
     public void testParseAssignments() {
         doTest(true);
     }
 
+    @Test
     public void testParseBoolean() {
         doTest(true);
     }
 
+    @Test
     public void testParseComments() {
         doTest(true);
     }
 
+    @Test
     public void testParseComparisonExpressions() {
         doTest(true);
     }
 
+    @Test
     public void testParseControlStructures() {
         doTest(true);
     }
 
+    @Test
     public void testParseFactorialExpressions() {
         doTest(true);
     }
 
+    @Test
     public void testParseGroupBy() {
         doTest(true);
     }
 
+    @Test
     public void testParseInterpolatedStatements() {
         doTest(true);
     }
 
+    @Test
     public void testParseLogicalExpressions() {
         doTest(true);
     }
 
+    @Test
     public void testParseMathExpressions() {
         doTest(true);
     }
 
+    @Test
     public void testParseMerge() {
         doTest(true);
     }
 
+    @Test
     public void testParseModifierNode() {
         doTest(true);
     }
 
+    @Test
     public void testParseNoParse() {
         doTest(true);
     }
 
+    @Test
     public void testParseNumbers() {
         doTest(true);
     }
 
+    @Test
     public void testParseOrderBy() {
         doTest(true);
     }
 
+    @Test
     public void testParsePhp() {
         doTest(true);
     }
 
+    @Test
     public void testParsePluck() {
         doTest(true);
     }
 
+    @Test
     public void testParsePropertyAccess() {
         doTest(true);
     }
 
+    @Test
     public void testParseSkip() {
         doTest(true);
     }
 
+    @Test
     public void testParseStrings() {
         doTest(true);
     }
 
+    @Test
     public void testParseSubexpressions() {
         doTest(true);
     }
 
+    @Test
     public void testParseSwitch() {
         doTest(true);
     }
 
+    @Test
     public void testParseTake() {
         doTest(true);
     }
 
+    @Test
     public void testParseVariables() {
         doTest(true);
     }
 
+    @Test
     public void testParseWhere() {
         doTest(true);
     }
