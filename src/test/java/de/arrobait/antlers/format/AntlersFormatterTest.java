9c8f5a9683baa40af1141d8a8d30fc0526b51ba2
Stefano Kowalke <info@arroba-it.de>
2022-12-11 17:21:36 +0100

    Add Formatter test

---
+++ src/test/java/de/arrobait/antlers/format/AntlersFormatterTest.java
@@ -0,0 +1,37 @@
+package de.arrobait.antlers.format;
+
+import com.intellij.psi.formatter.FormatterTestCase;
+import de.arrobait.antlers.file.AntlersFileType;
+import de.arrobait.antlers.util.AntlersTestUtils;
+
+public class AntlersFormatterTest extends FormatterTestCase {
+
+    @Override
+    protected String getBasePath() {
+        return "format";
+    }
+
+    @Override
+    protected String getFileExtension() {
+        return AntlersFileType.DEFAULT_EXTENSION;
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return AntlersTestUtils.BASE_TEST_DATA_PATH;
+    }
+
+    @Override
+    protected void doTest(String resultNumber) throws Exception {
+        String testName = getTestName(false);
+        doTest(testName + "." + getFileExtension(), testName + "_after." + getFileExtension(), resultNumber);
+    }
+
+    public void testHTML() throws Exception {
+        doTest();
+    }
+
+    public void testSimpleAntlers() throws Exception {
+        doTest();
+    }
+}
