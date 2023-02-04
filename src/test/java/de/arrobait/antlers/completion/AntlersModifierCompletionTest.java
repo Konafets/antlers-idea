06ffdb2e70eaed21319e159926daae8d41dc2b9b
Stefano Kowalke <info@arroba-it.de>
2023-02-04 13:45:44 +0000

    Add new modifiers
    - add_query_param
    - alias
    - antlers
    - bard_html
    - bard_items
    - bard_text
    - bools_string
    - dd
    - ddd
    - dump
    - diff_for_humans
    - diff_for_owls
    - extension
    - format_translated
    - is_alpha
    - is_array
    - is_iterable
    - joinplode
    - kebab
    - key_by
    - localize
    - mark
    - neatify
    - parse_url
    - pathinfo
    - pluck
    - random
    - regex_mark
    - remove_query_param
    - scope
    - set_query_param
    - shrug
    - split
    - str_pad
    - str_pad_both
    - str_pad_left
    - str_pad_right
    - studly
    - timestamp
    - to_bool
    - to_string
    - trackable_embed_url
    - type_of

--- src/test/java/de/arrobait/antlers/completion/AntlersModifierCompletionTest.java
+++ src/test/java/de/arrobait/antlers/completion/AntlersModifierCompletionTest.java
@@ -16,11 +16,11 @@ public class AntlersModifierCompletionTest extends BasePlatformTestCase {
     }
 
     public void testCompleteModifiersWithA() {
-        doBasicTest("{{ foo | a<caret>", "add", "add_slashes", "ampersand_list", "as", "ascii", "at");
+        doBasicTest("{{ foo | a<caret>", "add", "add_query_param", "add_slashes", "alias", "ampersand_list", "antlers", "ascii", "as", "at");
     }
 
     public void testCompleteModifiersWithB() {
-        doBasicTest("{{ foo | b<caret>", "background_position", "backspace");
+        doBasicTest("{{ foo | b<caret>", "background_position", "backspace", "bard_html", "bard_items", "bard_text", "bool_string");
     }
 
     public void testCompleteModifiersWithC() {
@@ -28,15 +28,15 @@ public class AntlersModifierCompletionTest extends BasePlatformTestCase {
     }
 
     public void testCompleteModifiersWithD() {
-        doBasicTest("{{ foo | d<caret>", "dashify", "days_ago", "decode", "deslugify", "divide", "dl", "dump");
+        doBasicTest("{{ foo | d<caret>", "dashify", "days_ago", "dd", "ddd", "debug", "decode", "deslugify", "diff_for_humans", "diff_for_owls", "divide", "dl", "dump");
     }
 
     public void testCompleteModifiersWithE() {
-        doBasicTest("{{ foo | e<caret>", "embed_url", "ends_with", "ensure_left", "ensure_right", "entities", "excerpt", "explode");
+        doBasicTest("{{ foo | e<caret>", "embed_url", "ends_with", "ensure_left", "ensure_right", "entities", "excerpt", "explode", "extension");
     }
 
     public void testCompleteModifiersWithF() {
-        doBasicTest("{{ foo | f<caret>", "favicon", "first", "flatten", "flip", "floor", "format", "format_localized", "format_number", "full_urls");
+        doBasicTest("{{ foo | f<caret>", "favicon", "first", "flatten", "flip", "floor", "format", "format_localized", "format_number", "format_translated", "full_urls");
     }
 
     public void testCompleteModifiersWithG() {
@@ -48,23 +48,27 @@ public class AntlersModifierCompletionTest extends BasePlatformTestCase {
     }
 
     public void testCompleteModifiersWithI() {
-        doBasicTest("{{ foo | i<caret>", "image", "in_array", "insert", "is_after", "is_alphanumeric", "is_before", "is_between", "is_blank", "is_email", "is_embeddable", "is_empty", "is_future", "is_json", "is_leap_year", "is_lowercase", "is_numberwang", "is_numeric", "is_past", "is_today", "is_tomorrow", "is_uppercase", "is_url", "is_weekday", "is_weekend", "is_yesterday", "iso_format");
+        doBasicTest("{{ foo | i<caret>", "image", "in_array", "insert", "is_after", "is_alpha", "is_alphanumeric", "is_array", "is_before", "is_between", "is_blank", "is_email", "is_embeddable", "is_empty", "is_future", "is_iterable", "is_json", "is_leap_year", "is_lowercase", "is_numberwang", "is_numeric", "iso_format", "is_past", "is_today", "is_tomorrow", "is_uppercase", "is_url", "is_weekday", "is_weekend", "is_yesterday");
     }
 
     public void testCompleteModifiersWithJ() {
-        doBasicTest("{{ foo | j<caret>", "join");
+        doBasicTest("{{ foo | j<caret>", "join", "joinplode");
+    }
+
+    public void testCompleteModifiersWithK() {
+        doBasicTest("{{ foo | k<caret>", "kebab", "key_by");
     }
 
     public void testCompleteModifiersWithL() {
-        doBasicTest("{{ foo | l<caret>", "last", "lcfirst", "length", "limit", "link", "list", "lower");
+        doBasicTest("{{ foo | l<caret>", "last", "lcfirst", "length", "limit", "link", "list", "localize", "lower");
     }
 
     public void testCompleteModifiersWithM() {
-        doBasicTest("{{ foo | m<caret>", "macro", "mailto", "markdown", "md5", "merge", "minutes_ago", "mod", "modify_date", "months_ago", "multiply");
+        doBasicTest("{{ foo | m<caret>", "macro", "mailto", "mark", "markdown", "md5", "merge", "minutes_ago", "mod", "modify_date", "months_ago", "multiply");
     }
 
     public void testCompleteModifiersWithN() {
-        doBasicTest("{{ foo | n<caret>", "nl2br");
+        doBasicTest("{{ foo | n<caret>", "neatify", "nl2br");
     }
 
     public void testCompleteModifiersWithO() {
@@ -72,19 +76,19 @@ public class AntlersModifierCompletionTest extends BasePlatformTestCase {
     }
 
     public void testCompleteModifiersWithP() {
-        doBasicTest("{{ foo | p<caret>", "path_info", "pad", "partial", "piped", "plural");
+        doBasicTest("{{ foo | p<caret>", "pad", "parse_url", "partial", "pathinfo", "pluck", "piped", "plural");
     }
 
     public void testCompleteModifiersWithR() {
-        doBasicTest("{{ foo | r<caret>", "raw", "rawurlencode", "ray", "read_time", "regex_replace", "relative", "remove_left", "remove_right", "repeat", "replace", "reverse", "round");
+        doBasicTest("{{ foo | r<caret>", "random", "raw", "rawurlencode", "ray", "read_time", "regex_mark", "regex_replace", "relative", "remove_left", "remove_query_param", "remove_right", "repeat", "replace", "reverse", "round");
     }
 
     public void testCompleteModifiersWithS() {
-        doBasicTest("{{ foo | s<caret>", "safe_truncate", "sanitize", "seconds_ago", "segment", "sentence_list", "shuffle", "singular", "slugify", "smartypants", "sort", "spaceless", "starts_with", "strip_tags", "substr", "subtract", "sum", "surround", "swap_case");
+        doBasicTest("{{ foo | s<caret>", "scope", "safe_truncate", "sanitize", "seconds_ago", "segment", "sentence_list", "set_query_parm", "shrug", "shuffle", "singular", "slugify", "smartypants", "sort", "spaceless", "split", "starts_with", "strip_tags", "str_pad", "str_pad_both", "str_pad_left", "str_pad_right", "studly", "substr", "subtract", "sum", "surround", "swap_case");
     }
 
     public void testCompleteModifiersWithT() {
-        doBasicTest("{{ foo | t<caret>", "table", "tidy", "timezone", "title", "to_json", "to_spaces", "to_tabs", "trans", "trim", "truncate");
+        doBasicTest("{{ foo | t<caret>", "table", "tidy", "timestamp", "timezone", "title", "to_bool", "to_json", "to_spaces", "to_string", "to_tabs", "trackable_embed_url", "trans", "trans_choice", "trim", "truncate", "type_of");
     }
 
     public void testCompleteModifiersWithU() {
