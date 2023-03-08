1ded01e7fd1dba0c958474adb99c12a4c4f0b8b5
Stefano Kowalke <info@arroba-it.de>
2023-03-08 15:35:11 -0700

    Fix and add more tests

--- src/main/java/de/arrobait/antlers/editor/structure/AntlersStructureViewFactory.java
+++ src/main/java/de/arrobait/antlers/editor/structure/AntlersStructureViewFactory.java
@@ -1,7 +1,6 @@
 package de.arrobait.antlers.editor.structure;
 
 import com.intellij.ide.structureView.StructureViewBuilder;
-import com.intellij.ide.structureView.impl.TemplateLanguageStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
 import com.intellij.psi.PsiFile;
 import de.arrobait.antlers.psi.AntlersFile;
